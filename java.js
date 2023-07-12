cda75d6761159e6f9af44da62590fe3ccfe8db0c
Slothgramer <santiagonolte@hotmail.com>
2023-07-12 11:22:36 -0400

    Finish calculator

--- java.js
+++ java.js
@@ -1,92 +1,91 @@
-let resultLine = document.getElementById("resultado");
-resultLine.textContent="Santiago";
-resultLine.style.textAlign = "right";
-resultLine.style.verticalAlign = "center";
-resultLine.style.fontSize = "30px";
-resultLine.style.paddingRight= "2.5px";
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
-let c = document.getElementById("c");
-c.addEventListener("click",function(){
-    resultLine.textContent = "0"});
+document.addEventListener("DOMContentLoaded",function(){
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+let decimal = document.querySelector(".decimal");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let previous = document.querySelector(".previous");
+let current = document.querySelector(".current");
 
-let seven = document.getElementById("seven");
-seven.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "7"}
+numbers.forEach((number)=> number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    current.textContent = currentValue;
+}))
+
+    operators.forEach((op)=> op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previous.textContent = previousValue + " "+operator;
+        current.textContent = currentValue;
+    })) 
+
+    clear.addEventListener("click", function(){
+        previousValue = "";
+        currentValue = "";
+        operator = '';
+        previous.textContent = currentValue;
+        current.textContent = currentValue; 
+    })
+
+    equal.addEventListener("click",function(){
+        if(currentValue != '' && previousValue != ''){
+            calculate()
+            previous.textContent = '';
+            if(previousValue.lenght <= 5){
+            current.textContent = previousValue;}
+            else{
+            current.textContent = previousValue.slice(0,5) + "...";
+            }
+        }
+    })
+
+    decimal.addEventListener("click",function(){
+        addDecimal();
+    })
+})
+
+function handleNumber(num){
+    if(currentValue.length <=5){
+    currentValue += num;
+    }
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue= '';
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    }
+    else if(operator === "-"){
+        previousValue -= currentValue;
+    }
+    else if(operator === "x"){
+        previousValue *= currentValue;
+    }
     else{
-    resultLine.textContent = resultLine.textContent + "7"}
-});
+        previousValue /= currentValue;
+    }
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
 
-let eight = document.getElementById("eight");
-eight.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "8"}
-    else{
-    resultLine.textContent = resultLine.textContent + "8"}
-});
+function roundNumber(num){
+    return Math.round(num*1000)/1000;
+}
 
-let nine = document.getElementById("nine");
-nine.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "9"}
-    else{
-    resultLine.textContent = resultLine.textContent + "9"}
-});
-
-let four = document.getElementById("four");
-four.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "4"}
-    else{
-    resultLine.textContent = resultLine.textContent + "4"}
-});
-
-let five = document.getElementById("five");
-five.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "5"}
-    else{
-    resultLine.textContent = resultLine.textContent + "5"}
-});
-
-let six = document.getElementById("six");
-six.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "6"}
-    else{
-    resultLine.textContent = resultLine.textContent + "6"}
-});
-
-let one = document.getElementById("one");
-one.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "1"}
-    else{
-    resultLine.textContent = resultLine.textContent + "1"}
-});
-
-let two = document.getElementById("two");
-two.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "2"}
-    else{
-    resultLine.textContent = resultLine.textContent + "2"}
-});
-
-let three = document.getElementById("three");
-three.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "3"}
-    else{
-    resultLine.textContent = resultLine.textContent + "3"}
-});
-
-let zero = document.getElementById("zero");
-zero.addEventListener("click",function(){
-    if(resultLine.textContent.charAt(0)==0){resultLine.textContent = "0"}
-    else{
-    resultLine.textContent = resultLine.textContent + "0"}
-});
-
-let dot = document.getElementById("dot");
-dot.addEventListener("click",function(){
-    if(resultLine.textContent.chartAt(0)==0){
-    resultLine.textContent = resultLine.textContent + "."}
-});
-
-
-let equal = document.getElementById("equal");
-equal.addEventListener("click",function(){
-    resultLine.textContent = "total"
-});
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
+}
